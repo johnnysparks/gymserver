3d1396ff420de66d8e9f741ac20bcf6b4140f913
johnnyfuchs <johnnyfuchs@gmail.com>
2012-08-20 01:57:28 -0500

    not using cloudmine anymore

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,36 @@
-var WebService = require('../lib/cloudmine-0.9.js'),
-    // email services
-    SendGrid = require('sendgrid').SendGrid,
+// email services
+var SendGrid = require('sendgrid').SendGrid,
     Email    = require('sendgrid').Email,
     CONFIG   = require('../config'),
-    Parse    = require('../lib/parse-1.0.15.js').Parse;
+    Parse    = require('../lib/parse-1.0.15.js').Parse,
+    Emailer  = require('../lib/classes/emailer.js');
 
 Parse.initialize( CONFIG.parse.appid, CONFIG.parse.jsKey );
 
-var sendgrid = new SendGrid( CONFIG.sendgrid  );
-var wc       = new WebService( CONFIG.cloudmine );
 
+
+/**
+ * SEND EMAILS
+ **/
+exports.sendemail = function(req, res){
+  var req.body = req.body || {};
+  var to_addrs = ['johnny@daily.do', 'johnnyfuchs@gmail.com', 'johnny.fuchs@shoutlet.com'];
+//  var to_addrs = [ 'hgrigg@supremehealthfitness.com', 'shannon@supremehealthfitness.com', 'johnnyfuchs@gmail.com'];
+
+  var emailer = new Emailer();
+  emailer.to(to_addrs);
+  emailer.to(req.body.to);
+  emailer.from(req.body.from);
+  emailer.subject(req.body.subject);
+  emailer.body(req.body.body);
+  emailer.attach(req.body.attName, req.body.attBody);
+  emailer.send(function(response){
+    res.send(response);
+  });
+}
+
+
+exports.login = function(req, res){ 
 var TestObj  = Parse.Object.extend('TestObj');
 var t = new TestObj();
 
@@ -20,70 +41,6 @@ t.save({
 }, {
   success: function(o){ console.log(o); }
 });
-
-/**
- * SEND EMAILS
- **/
-exports.sendemail = function(req, res){
-  var to_addrs = ['johnny@daily.do', 'johnnyfuchs@gmail.com', 'johnny.fuchs@shoutlet.com'];
-//  var to_addrs = [ 'hgrigg@supremehealthfitness.com', 'shannon@supremehealthfitness.com', 'johnnyfuchs@gmail.com'];
-  var xhr    = {"status": "error", "message": "Failed to send email.", "data": {}};
-  var params = req.body || {};
-  console.log("params are");
-  console.log(params);
-
-
-  // validate emails
-  if( !params.from || !validishEmail(params.from)){ res.send( xhr ); res.end(); return true; }
-
-  // data to be passed to sendgrid servers
-  var email = new Email({
-      to       : params.to,
-      from     : params.from,
-      subject  : params.subject,
-      text     : params.body,
-  });
-
-  // add the extra emails
-  for(var i in to_addrs){
-    email.addTo(to_addrs[i]);
-  }
-
-  if( params.attName && params.attBody ){
-    email.addFile({
-      filename : params.attName,
-      content  : new Buffer(params.attBody)
-    });
-  }
-
-  console.log("getting to send email");
-  // Fire off the sendgrid email
-  sendgrid.send( email, function(success, message){
-      if(!success){
-        xhr.message = "Email Server Error.";
-        xhr.data    = message;
-      } else {
-        xhr.status  = "success";
-        xhr.message = "Email Sent";
-        xhr.data    = message;
-      }
-      res.send(xhr);
-  });
-
-  /**
-   * The most basic of email validators,
-   * sendgrid will fail if it "looks" valid but isn't anyway
-   **/
-  function validishEmail(str) {
-      var lastAtPos = str.lastIndexOf('@');
-      var lastDotPos = str.lastIndexOf('.');
-      return (lastAtPos < lastDotPos && lastAtPos > 0 && str.indexOf('@@') == -1 && lastDotPos > 2 && (str.length - lastDotPos) > 2);
-  }
-  return true;
-}
-
-
-exports.login = function(req, res){ 
 
 
 }
